5c2f0faf0ad770c1d759b78e315daaf305ba9de9
machsh64 <machshren@gmail.com>
2023-01-30 00:06:32 +0800

    Site updated: 2023-01-30 00:06:31

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,4 +1,3 @@
-//live2d_path 参数建议使用绝对路径
 //const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 const live2d_path = "/live2d/";
 
@@ -27,7 +26,7 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "waifu-right.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
