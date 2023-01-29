80b41f9a20a8325d994ba9a4b398d203258d8a05
machsh64 <machshren@gmail.com>
2023-01-30 02:24:19 +0800

    Site updated: 2023-01-30 02:24:18

--- live2d/src/tools.js
+++ live2d/src/tools.js
@@ -13,7 +13,6 @@ function showHitokoto() {
     fetch("https://v1.hitokoto.cn")
         .then(response => response.json())
         .then(result => {
-            const text = `这句一言来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`;
             showMessage(result.hitokoto, 6000, 9);
             setTimeout(() => {
                 showMessage(text, 4000, 9);
@@ -53,24 +52,6 @@ const tools = {
             showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
             Live2D.captureName = "photo.png";
             Live2D.captureFrame = true;
-        }
-    },
-    "info": {
-        icon: fa_info_circle,
-        callback: () => {
-            open("https://github.com/stevenjoezhang/live2d-widget");
-        }
-    },
-    "quit": {
-        icon: fa_xmark,
-        callback: () => {
-            localStorage.setItem("waifu-display", Date.now());
-            showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
-            document.getElementById("waifu").style.bottom = "-500px";
-            setTimeout(() => {
-                document.getElementById("waifu").style.display = "none";
-                document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
-            }, 3000);
         }
     }
 };
