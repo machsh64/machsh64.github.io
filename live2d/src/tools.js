ac0ab8c7b9bade34fd4cfe4b0707095666017560
machsh64 <machshren@gmail.com>
2023-01-29 23:19:21 +0800

    Site updated: 2023-01-29 23:19:20

--- live2d/src/tools.js
+++ live2d/src/tools.js
@@ -13,7 +13,7 @@ function showHitokoto() {
     fetch("https://v1.hitokoto.cn")
         .then(response => response.json())
         .then(result => {
-            const text = `这句一言来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`;
+            /* const text = `这句一言来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`; */
             showMessage(result.hitokoto, 6000, 9);
             setTimeout(() => {
                 showMessage(text, 4000, 9);
