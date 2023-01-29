534ebd393334eb9596bcad5955b94ecb1dfc025c
machsh64 <machshren@gmail.com>
2023-01-30 05:02:35 +0800

    Site updated: 2023-01-30 05:02:34

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,41 +1,15 @@
-const live2d_path = "/live2d/";
-
-// 封装异步加载资源的方法
-function loadExternalResource(url, type) {
-	return new Promise((resolve, reject) => {
-		let tag;
-
-		if (type === "css") {
-			tag = document.createElement("link");
-			tag.rel = "stylesheet";
-			tag.href = url;
-		}
-		else if (type === "js") {
-			tag = document.createElement("script");
-			tag.src = url;
-		}
-		if (tag) {
-			tag.onload = () => resolve(url);
-			tag.onerror = () => reject(url);
-			document.head.appendChild(tag);
-		}
-	});
-}
-
-// 加载 waifu.css live2d.min.js waifu-tips.js
-if (screen.width >= 368) {
-	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
-	]).then(() => {
-		// 配置选项的具体用法见 README.md
-		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://live2d.fghrsh.net/api/",
-			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
-			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo"]
-		});
-	});
-}
-
+try {
+    $("<link>").attr({href: "assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
+    $.ajax({url: "assets/waifu-tips.min.js?v=1.4.2", dataType:"script", cache: true, success: function() {
+        $.ajax({url: "assets/live2d.min.js?v=1.0.5", dataType:"script", cache: true, success: function() {
+            /* 可直接修改部分参数 */
+            live2d_settings['hitokotoAPI'] = "hitokoto.cn";  // 一言 API
+            live2d_settings['modelId'] = 5;                  // 默认模型 ID
+            live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
+            live2d_settings['modelStorage'] = false;         // 不储存模型 ID
+            /* 在 initModel 前添加 */
+            initModel("assets/waifu-tips.json");
+        }});
+    }});
+} catch(err) { console.log("[Error] JQuery is not defined.") }
